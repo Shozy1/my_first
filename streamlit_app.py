25acea338698242ab8e4316caed9cc59f0031cee
Shozy1 <eshiozemheinusa@gmail.com>
2024-09-30 22:38:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ st.title('Forcasting External Sector Variables')
 
 st.info('Become an External Sector Expert')
 With st.expander("Data"):
-  st.write('**Rae data**)
+  st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/Shozy1/my_first/master/EXR.csv')
   df
