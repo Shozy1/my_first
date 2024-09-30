3feb879f6a8108f0332ad187b95b2ea9bdc5bd08
Shozy1 <eshiozemheinusa@gmail.com>
2024-09-30 22:49:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ with st.expander("Data"):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/Shozy1/my_first/master/EXR.csv')
   data = df.drop(df.columns[13:], axis=1)
-
   data = data.drop(['BDC'], axis=1)
+  data
