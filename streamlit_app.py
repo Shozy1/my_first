a9adaa9a3b76d74781d771820391a1d2cbfc0b56
Shozy1 <eshiozemheinusa@gmail.com>
2024-09-30 22:44:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ st.info('Become an External Sector Expert')
 with st.expander("Data"):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/Shozy1/my_first/master/EXR.csv')
-  df
+  data = df.drop([BDC, 13: ] axis=1)
