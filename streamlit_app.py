7105d76c9d6f85711e49eadac89e4a1bd5b12d16
Shozy1 <eshiozemheinusa@gmail.com>
2024-10-01 00:29:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import openai 
-
-
-
 
 #Title of the work
 st.title('Forcasting External Sector Variables')
