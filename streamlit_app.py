bdc25ec0913c091bbf0269384bed36494f9348db
Shozy1 <eshiozemheinusa@gmail.com>
2024-09-30 22:31:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.title('Forcasting External Sector Variables')
 
 st.info('Become an External Sector Expert')
 
-df = pd.read_csv('
+df = pd.read_csv('https://github.com/Shozy1/my_first/blob/master/EXR.csv')
